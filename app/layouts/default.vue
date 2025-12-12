67a80a38c3458d4725ef3232097ba1cceb381038
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-12 15:47:35 +0200

    creates lyouts, pages and components folders and files in those folders

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppHeader from "@/components/app-header.vue";
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
+</template>
