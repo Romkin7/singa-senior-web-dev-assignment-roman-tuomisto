4eab3fed4a7d110830d4911628238738980e2586
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-13 18:11:28 +0200

    creates reusable component, search-form

---
+++ app/components/search-form.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const queryString = defineModel<string>("query-string");
+const emit = defineEmits<{
+  "search-genres": [];
+}>();
+</script>
+<template>
+  <form @submit.prevent="emit('search-genres')">
+    <input
+      name="name"
+      v-model="queryString"
+      type="search"
+      placeholder="Search genres"
+    />
+    <button type="submit">Search</button>
+  </form>
+</template>
