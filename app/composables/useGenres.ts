035c1bf8e77fe286b2b8572cae5c06ad55058cf9
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-13 15:38:21 +0200

    return whole response from useGenres composable and only genres array and utilizes signals in composable itself

--- app/composables/useGenres.ts
+++ app/composables/useGenres.ts
@@ -1,11 +1,10 @@
-import type { SingaGenresResponse } from "@/@types/singaGenresResponse";
+import type { AsyncData, NuxtError } from "#app";
 
 /**
  * useGenres composable that,
  * takes in apiBaseUri and pageSize as optional parameter,
  * that defaults to 38 items,
- * fetches data from Singa api
- * and returns SingaGenresResponse.
+ * fetches data from Singa api.
  *
  * Uses /genres as hardcoded path,
  * because this composable is specifically meant to fetch Singa Genres,
@@ -15,14 +14,13 @@ import type { SingaGenresResponse } from "@/@types/singaGenresResponse";
  * thus it's better to configure it in nuxt.config.ts.
  * @param {string} apiBaseUri
  * @param {number|undefined} pageSize
- * @returns {Promise<SingaGenresResponse>}
+ * @returns {PromiseAsyncData<unknown, NuxtError<unknown> | undefined>}
  */
-export const useGenres = async (
+export const useGenres = (
   apiBaseUri: string,
   pageSize: number = 38
-): Promise<SingaGenresResponse> => {
-  const { data } = await useAsyncData("singa-genres", () =>
-    $fetch(`${apiBaseUri}/genres?page_size=${pageSize}`)
+): AsyncData<unknown, NuxtError<unknown> | undefined> => {
+  return useAsyncData("singa-genres", (_nuxtApp, { signal }) =>
+    $fetch(`${apiBaseUri}/genres?page_size=${pageSize}`, { signal })
   );
-  return data.value as SingaGenresResponse;
 };
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
+import type { SingaGenresResponse } from "~/@types/singaGenresResponse";
+
 const runtimeConfig = useRuntimeConfig();
 const apiBaseUri = runtimeConfig.public.apiBaseUri;
-const { results } = await useGenres(apiBaseUri);
-const genres = results;
+
+const { data, pending, error, refresh } = await useGenres(apiBaseUri);
+
+// save results as genres, uses computed, to update view and data based using
+const genres = computed(
+  () => (data.value as SingaGenresResponse)?.results || []
+);
 </script>
 <template>
   <section>
     <h1>Genres</h1>
-    <div class="grid-row">
+    <div class="grid-row" v-if="pending">
+      <div class="grid-col">
+        <h2>Loading</h2>
+      </div>
+    </div>
+    <div class="grid-row" v-else-if="error">
+      <div class="grid-col">
+        <h2>Error</h2>
+        <p>Status: {{ error.statusCode }}</p>
+        <p>{{ error.message }}</p>
+      </div>
+    </div>
+    <div class="grid-row" v-else-if="genres.length === 0">
+      <div class="grid-col">
+        <h2>No Genres to display</h2>
+      </div>
+    </div>
+    <div class="grid-row" v-else>
       <div class="grid-col" v-for="genre in genres" :key="genre.id">
         <h2>{{ genre.name }}</h2>
         <p>id {{ genre.id }}</p>
