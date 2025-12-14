1583f5fc1b513698217ffdc6a87dd1e154a65dd0
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-14 19:56:10 +0200

    adds useFetch to call for /api/ping endpoint to check server health

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+const { data, error } = await useFetch("/api/ping");
+if (error.value) {
+  throw createError({
+    statusCode: 500,
+    statusMessage: "Failed to fetch /ping endpoint",
+  });
+} else {
+  console.log("Response from /ping:", data.value);
+}
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
