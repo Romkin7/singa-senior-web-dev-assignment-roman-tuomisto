eef1bd71e7d7e74a4fc6aeee3b1799522543f362
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-14 15:08:13 +0200

    creates about page

---
+++ app/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1>About This App</h1>
+    <p>
+      This application is built using Nuxt 3 and TypeScript. It showcases a list
+      of genres fetched from an external API, allowing users to search and view
+      details about each genre.
+    </p>
+    <p>
+      Developed by Roman Tuomisto as part of the Singa Senior Web Developer
+      assignment.
+    </p>
+  </main>
+</template>
